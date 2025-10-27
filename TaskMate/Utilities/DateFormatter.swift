4d302174f789abf89b82d8382a4b743a8e9d9d6b
Alexander <lef0791@gmail.com>
2025-10-27 18:43:52 +0300

    Work on cell constraints and date formatter

---
+++ TaskMate/Utilities/DateFormatter.swift
@@ -0,0 +1,25 @@
+//
+//  DateFormatter.swift
+//  TaskMate
+//
+//  Created by aex on 27.10.2025.
+//
+
+import UIKit
+
+class DateFormatterHelper {
+    
+    static let shared = DateFormatterHelper()
+    
+    private init() {}
+    
+    func formatDate(from date: Date?) -> String {
+        guard let date = date else { return "" }
+        
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .none
+        
+        return formatter.string(from: date)
+    }
+}
